d0692007fa7de81518939774f9ba42549279d81c
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-04-27 00:52:21 +0300

    Implemented API

--- cinema/models.py
+++ cinema/models.py
@@ -6,5 +6,23 @@ class Movie(models.Model):
     description = models.TextField()
     duration = models.IntegerField()
 
+    actors = models.ManyToManyField("Actor", related_name="movies")
+    genres = models.ManyToManyField("Genre", related_name="movies")
+
     def __str__(self):
         return self.title
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
